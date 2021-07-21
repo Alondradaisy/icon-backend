943bfb6942a577f82109ca22a1a7d6e38ead908e
Alondradaisy <alondra.warren96@gmail.com>
2021-07-21 12:10:05 -0700

    bring in userRouter and require paths within helpFuncs,authMiddleware, jwtMiddleware and add router reqs

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,18 +1,50 @@
 const express = require("express");
 const router = require.Router();
 
-const { createProfile } = require("./controller/userController");
+const {
+  createProfile,
+  login,
+  fetchUserInfo,
+} = require("./controller/userController");
 
 //place helperFuncs in const variables and connect to their paths
+const scanIsUndefinedHelper = require("./helperFuncs/scanIsUndefinedHelper");
+const scanIsEmptyHelper = require("./helperFuncs/scanIsEmptyHelper");
+const scanIsStrongPasswordHelper = require("./helperFuncs/scanIsStrongPasswordHelper");
 
 //require helperFuncs in helperFuncs -> authMiddleware
+
+const {
+  scanIsEmailHelper,
+  scanIsAlphaHelper,
+  scanIsAlphaNumericHelper,
+} = require("./helperFuncs/authMiddleware");
 
 const jwtMiddleware = require("../utils/jwtMiddleware");
 
 //create validator router.posts for ->createProfile
-router.post();
+router.post(
+  "/create-profile",
+  scanIsUndefinedHelper,
+  scanIsEmptyHelper,
+  scanIsEmailHelper,
+  scanIsAlphaHelper,
+  scanIsStrongPasswordHelper,
+  scanIsAlphaNumericHelper,
+  createProfile
+);
 
 //create validator router.posts for -> Login
-router.post();
+router.post(
+  "/login",
+  scanIsUndefinedHelper,
+  scanIsEmptyHelper,
+  scanIsEmailHelper,
+  login
+);
+
+router.get("/get-user-info", jwtMiddleware, fetchUserInfo);
+
+router.put("");
 
 module.exports = router;
