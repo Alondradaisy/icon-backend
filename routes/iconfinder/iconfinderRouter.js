17523f4a6a3635a92c6ab71353f601fe01b87183
Alondradaisy <alondra.warren96@gmail.com>
2021-07-26 14:06:17 -0700

    work through iconRouter

--- routes/iconfinder/iconfinderRouter.js
+++ routes/iconfinder/iconfinderRouter.js
@@ -3,19 +3,26 @@ const router = express.Router();
 const jwtMiddleWare = require("../utils/jwtMiddleware");
 
 const apiKey = process.env.API_KEY;
-const clientId = process.env.CLIENT_ID;
 const icons = require("iconFinder")(apiKey, clientId);
 
-const url = "https://api.iconfinder.com/v4/icons/search?query=social&count=10";
+// const url = "https://api.iconfinder.com/v4/icons/search?query=social&count=10";
 
-const options = {
-  method: "GET",
+axios.get("https://api.iconfinder.com/v4/icons/search?query=arrow&count=10", {
   headers: {
     Accept: "application/json",
     Authorization:
-      "Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1",
+      "Bearer zIrNU9HJBdceao7UnIyCVNZ3AbZVsUw4e46ONPUoSusZnUacilp5LYZBBHHi7TeO",
   },
-};
+});
+// const options = {
+//   method: "GET",
+//   headers: {
+//     Accept: "application/json",
+//     Authorization:
+//       "Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1",
+//   },
+// };
+let results;
 
 fetch(url, options)
   .then((res) => res.json(icons))
